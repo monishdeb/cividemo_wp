a759f85327fac21ec9de665cff749cbdb88c1b22
Monish Deb <monish.deb@jmaconsulting.biz>
2024-05-28 11:37:22 +0530

    Add files

---
+++ wp-content/plugins/civicrm/civicrm/civicrm-version.php
@@ -0,0 +1,8 @@
+<?php
+/** @deprecated */
+function civicrmVersion( ) {
+  return array( 'version'  => '5.73.3',
+                'cms'      => 'Wordpress',
+                'revision' => '' );
+}
+
---
+++ wp-content/plugins/civicrm/civicrm/packages/smarty5/vendor/composer/installed.php
@@ -0,0 +1,43 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '84b16439666b5ec85ad89de609425ccbfbbec106',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '84b16439666b5ec85ad89de609425ccbfbbec106',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'smarty/smarty' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'beafa5ec31f2ca241a67ba9d04d868ba8596f62f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../smarty/smarty',
+            'aliases' => array(
+                0 => '5.0.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'symfony/polyfill-mbstring' => array(
+            'pretty_version' => 'v1.29.0',
+            'version' => '1.29.0.0',
+            'reference' => '9773676c8a1bb1f8d4340a62efe641cf76eda7ec',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symfony/polyfill-mbstring',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
